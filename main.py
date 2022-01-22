6128679bfd42b00f2a1c5c82ca9684dd4f31a2ac
Mikita Slabysh <slabysh2015@gmail.com>
2022-01-22 19:53:21 +0000

    Improved actions and inventory items, small bug fixes

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def start_room_moves_actions(player_input):
 def start_room_moves(player_input):
   global player_location, room_description, room_text, current_location_name
   if player_input == "left":
-    print("You walk out to the living room. You see dusty Tv in the edge of the room, you see stairs leading to the second floor. You also see bloody couch")
+    print("You walk out to the living room. You are in a strange living room, that looks like a maniac lived in. The floors are covered with dust, dirt, and empty beer cans. Tv is on, but it's only showing static. Couch is soaked in blood and covered with dirty clothes on top of it. On the floor near the couch you can see an ashtray full of cigarettes. On the table next to the couch you can see an old wired phone. To the right of yourself, you can see a door that looks like it's leading to the basement. To the left of you can see a door that is leading to garage. And you can also see a big metal front door that looks like it's leading outside")
     room_description = "You are in a strange living room, that looks like a maniac lived in. The floors are covered with dust, dirt, and empty beer cans. Tv is on, but it's only showing static. Couch is soaked in blood and covered with dirty clothes on top of it. On the floor near the couch you can see an ashtray full of cigarettes. On the table next to the couch you can see an old wired phone. To the right of yourself, you can see a door that looks like it's leading to the basement. To the left of you can see a door that is leading to garage. And you can also see a big metal front door that looks like it's leading outside."
     #switch rooms
     next_room("living_room", room_description, living_room, living_room_text)
@@ -79,7 +79,7 @@ def start_room_moves(player_input):
 
 def start_room_actions(player_input):
   if player_input == "light":
-    print("In darkness, you somehow manage to find a light switch, and turn it on. As the lights turn on, you realize that your finger, and lightswitch, and knife, and entire dining is in blood")
+    print("In darkness, you somehow manage to find a light switch, and turn it on. As the lights turn on, you realize that your finger, and lightswitch, and entire dining is in blood")
     start_room.keys["light"] = "on"
 
 #ROOM 1 END ------------------------------------------------
@@ -124,8 +124,8 @@ def kitchen_actions(player_input):
 
 #Kitchen End -----------------------------------------------------
 #Living Room Start -----------------------------------------------
-living_room_keys = {"front door" : "locked", "basement door" : "locked", "garage door" : "locked", "phone" : "off"}
-living_room = Location(("forward", "stairs", "right", "left", "basement door", "garage door", "back"), ("front door", "adjust TV", "Use phone"), living_room_keys)
+living_room_keys = {"front door" : "locked", "basement door" : "locked", "garage door" : "locked", "phone" : "off", "tv" : "off"}
+living_room = Location(("forward", "stairs", "right", "left", "basement door", "garage door", "back"), ("front door", "adjust TV", "use phone"), living_room_keys)
 living_room_text = Fore.LIGHTMAGENTA_EX + "\nCurrent Room: Living Room\n" + Fore.BLUE + "\nMoves: \n forward, right, left, back, basement door, garage door, stairs \nActions:\n front door, adjust TV, use phone\n"
 
 def living_room_moves_actions(player_input):
@@ -154,12 +154,15 @@ def living_room_moves(player_input):
   
 
 def living_room_actions(player_input):
+  global name
   if player_input == "front door" and living_room.keys["front door"] == "locked":
     print("You happily run to the front door, hoping your hell of a stay in this place will be over soon. But very soon your dreams gets crushed by the metal front door, that wont budge at all, and the only way to open it, is to use a key.")
   elif player_input == "front door" and living_room.keys["front door"] == "open":
     print("You open the door, and run away like a free bird")
-  elif player_input == "adjust TV":
+  elif player_input == "adjust TV" and living_room.keys["tv"] == "off":
     print("You fix antenna on top of the TV, and you adjust channel and volume buttons to broadcast local news channel. The news reporter was telling latest news: And to the other news, we are still searching for " + name + " who has disappeared yesterday night around 9 pm. They left for the bar at 8 30 pm, after 9 pm their phone wasn't available anymore. Last place they were seen is Red Light District. Witnesses say that a suspicios white van was driving around that district at that time. If you have any information related to this case, please call us at 404-394-8567")
+  elif player_input == "adjust TV" and living_room.keys["tv"] == "on":
+    print("TV is already on")
   elif player_input == "use phone" and living_room_keys["phone"] == "off":
     print("You take the phone of the stand and put it next to your ear, but all you can hear is empty static indicating that phone is not connected to the phone line. If you can find where to connect phone to the line, you can call someone for help.")
   elif player_input == "use phone" and living_room_keys["phone"] == "on":
@@ -171,54 +174,93 @@ def living_room_actions(player_input):
 basement_room_keys = {"phone" : "off"}
 basement_room = Location(("forward", "right", "left", "back"), ("fix phoneline","use computer", "grab shotgun"), basement_room_keys)
 basement_room_text = Fore.LIGHTMAGENTA_EX + "\nCurrent Room: Basement Room\n" + Fore.BLUE + "\nMoves: \n forward, right, left, back \nActions:\n fix phoneline, use computer, grab shotgun\n"
+
+def basement_room_moves_actions(player_input):
+  if player_input in basement_room.moves:
+    basement_room_moves(player_input)
+  elif player_input in basement_room.actions:
+    basement_room_actions(player_input)
+  else:
+    print("Invalid Move")
+    play_game()
+
+def basement_room_moves(player_input):
+  pass
+
+def basement_room_actions(player_input):
+  pass
 #Basement Room End ---------------------------------------------------------------------------
 #Inventory Actions
 def InventoryActions(player_input):
   global player_data, ShotgunAmmo
   hpRestore = 25
-
+  #Shotgun
   if player_input == "shotgun":
     print("You take the weapon in your hands and you look over it. So many things you can do with it, what will be one of those things ?")
     print("\n1.Check Ammo \n2. Shoot Locked Door\n3. Take yourself out of a misery")
     ch = int(input("Enter your choice: "))
+    #End your sufferings
     if(ch == 3):
       print("You took stirdy gun in your hands, you take one last look over it, after which you put the ice cold barrel in your mouth, close your eyes, think about last nice things that happened in your life, and with tears in your eyes, you press the trigger.BOOM! Loud shot splash your brains all over the wall")
       player_data["health"] = 0
+    #No Locked Doors to shoot
     elif (ch == 2 and player_data["location_name"] != "living_room"):
       print("No locked doors around")
+    #Shoot Locked Door
     elif ch == 2 and player_data["location_name"] == "living_room" and ShotgunAmmo > 0:
       print("\n1. Shoot Garage Door \n2. Shoot Basement Door")
       ch = int(input("Enter your choice: "))
+      #Garage Door
       if ch == 1:
         print("You blasted Garage Door our")
         ShotgunAmmo -= 1
         living_room.keys["garage door"] = "open"
+        #Basement Door
       elif ch == 2:
         print("You blasted Basement Door our")
         ShotgunAmmo -= 1
         living_room.keys["basement door"] = "open"
+    #NO AMMO
     elif ch == 2 and player_data["location_name"] == "living_room" and ShotgunAmmo <= 0:
       print("You attempt to shoot, but shotgun is empty.Find some bullets")
+    #Check Ammo
     elif ch == 1:
       print("You check ammo mag, and find " + str(ShotgunAmmo) + " Shotgun Shells")
-
+  #Medkit
   elif player_input == "medkit":
     if player_data["health"] < 100:
       print("You take out medkit that you found and you apply first aid to yourself. You gain 25 points of Health")
       player_data["health"] += hpRestore
+      inventory.remove("medkit")
       if player_data["health"] >= 100:
         player_data["health"] = 100
     else:
       print("You are feeling amazing, save your medkit for the future")
-
+  #Knife
   elif player_input == "knife":
     print("You take bloody knife out and take a look over it.")
-    print("\n1. Lock pick the Doorknob\n 2. Attack")
+    print("\n1. Lock pick the Doorknob\n2. Attack")
     ch = int(input())
-    if ch == 1 and player_data["location"] == "living_room":
+    #Choice for knife
+    if ch == 1 and player_data["location_name"] == "living_room":
       print("Which door would you like to open ? 1. Basement door 2. Garage Door")
-    elif ch == 1 and player_data["location"] != "living_room":
+      ch = int(input())
+      #Basement 
+      if ch == 1:
+        print("You took out a knife and fiddled with a door knob to unlock it. It took you a few minutes to lockpick it, but you broke the knife in the process.")
+        inventory.remove("knife")
+        living_room.keys["basement door"] = "open"
+      #Garage Door
+      elif ch == 2:
+        print("You took out a knife and fiddled with a door knob to unlock it. It took you a few minutes to lockpick it, but you broke the knife in the process.")
+        inventory.remove("knife")
+        living_room.keys["garage door"] = "open"
+    #No Door 
+    elif ch == 1 and player_data["location_name"] != "living_room":
       print("No doors to lockpick!")
+    #Attack
+    elif ch == 2:
+      print("Nobody to attack around you")
 
     
 
@@ -268,6 +310,8 @@ def play_game():
       kitchen_moves_actions(player_input)
     elif current_location_name == "living_room":
       living_room_moves_actions(player_input)
+    elif current_location_name == "basement_room":
+      basement_room_moves_actions(player_input)
 
   #Check if the game is over
   if player_data["health"] < 1:
